ee6d1389cfe1a33e608c269d8958977b8c9ca6df
J Pennington <grendelsdj@gmail.com>
2018-04-03 14:38:49 -0500

    removed some annotations

--- liri.js
+++ liri.js
@@ -1,27 +1,16 @@
 require("dotenv").config();
 
-var keys = require('./keys.js');
-
-// Get Node File System 
+var keys = require('./keys.js'); 
 var fs = require('fs');
-// Get Node Request
 var request = require('request');
-// Get Node Spotify
 var Spotify = require('node-spotify-api');
-// Get Node Twitter
 var Twitter = require('twitter');
-
 var spotify = new Spotify(keys.spotify);
-
 var client = new Twitter(keys.twitter);
-
-// Get Liri command and data from the command prompt
 
 var liriArgs = process.argv.slice(2);
 var liriCommand = liriArgs[0];
 var liriData = liriArgs[1];
-
-// Main function to process the Liri Commands and Data entered from the command prompt
 
 function liri(liriCommand, liriData) {
     switch (liriCommand) {
@@ -61,29 +50,13 @@ function liri(liriCommand, liriData) {
             console.log("no choice");
     }
 }
-
-// Function to process the Liri my-tweets command and show the first 20 twitts.
-
 function myTweets() {
-    // Read in the key file.
-    //var keys = require('./keys.js');
-    // Read in the keyList and an assign the keys to the Twitter client
-    //var keyList = keys.twitterKeys;
-
-    
-    //var client = new twitter({
-    //    consumer_key: keyList.consumer_key,
-    //    consumer_secret: keyList.consumer_secret,
-    //    access_token_key: keyList.access_token_key,
-    //    access_token_secret: keyList.access_token_secret
-   // });
-    // Assign the screen name and number of tweets to return.
     var params = {
         screen_name: liriData,
     };
-    // Get the tweets
+
     client.get('statuses/user_timeline.json', params, function(error, tweets, response) {
-        // Display the tweets and show error if there is an error
+       
         if (!error) {
             if (tweets.length < 20) {
                 var numberTweets = tweets.length;
@@ -101,12 +74,10 @@ function myTweets() {
     });
 }
 
-// Function to process the Liri spotify-this-song command and search for song.
+
 
 function spotifyThisSong(song) {
-    // Search for song 
     spotify.search({ type: 'track', query: song }, function(error, response) {
-        // Display song details and show error if there is an error
         if (!error) {
             console.log('Artist Name: ' + response.tracks.items[0].artists[0].name);
             console.log('Song Name: ' + response.tracks.items[0].name);
